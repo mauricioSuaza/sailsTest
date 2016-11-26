73bf6bdc9978919bca7e928218d97290a69ab3ec
MAURICIO SUAZA <mauriciosuazauribe@gmail.com>
2016-11-26 05:20:03 -0500

    changed views location

--- config/models.js
+++ config/models.js
@@ -18,7 +18,8 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
    //connection: 'localDiskDb',
-  connection: 'somemongo'
+  connection: 'somemongo',
+  migrate: 'safe'
   /***************************************************************************
   *                                                                          *
   * How and whether Sails will attempt to automatically rebuild the          *
